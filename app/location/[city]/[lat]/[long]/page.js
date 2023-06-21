b5a52ab2551ac733f77f6417b190591e46f43e7c
isuru <isurumanod99@gmail.com>
2023-06-21 13:02:41 +0530

    redesign report page for better user experience

---
+++ app/location/[city]/[lat]/[long]/page.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import {ApolloClient, InMemoryCache} from "@apollo/client";
+import ColloutCard from "@/components/ColloutCard";
+import StatCard from "@/components/StatCard";
+import InformationPanel from "@/components/InformationPanel";
+import TempChart from "@/components/TempChart";
+
+
+
+
+async function weather({params: {city, lat, long}}) {
+
+
+
+
+
+    return (
+        <div className={"min-h-screen flex flex-col md:flex-row "}>
+
+
+            <InformationPanel city={city} lat={lat} long={long} results={results}/>
+
+
+            <div className={"p-5 flex-1 lg:px-10 "}>
+                <div className={"pb-5"}>
+                    <div className={"pb-5"}>
+                        <h2 className={"text-xl font-bold"}>Today's Overview</h2>
+                        <p className={"text-sm text-gray-400"}>Last updated at
+                            : {new Date(results.current_weather.time).toLocaleString()}</p>
+                    </div>
+                    <div className={"m-2 mb-10"}><ColloutCard message={"This is where GPT-3.5 Summary will go !"}
+                                                              warning={false}/></div>
+                    <div className={"grid  grid-cols-1 lg:grid-cols-2 gap-5 m-2 "}>
+                        <StatCard title={"Maximum Temperature"}
+                                  metric={`${results.current_weather.temperature?.toFixed(1)} °C`}
+                                  color={"orange"}
+                        />
+                        <StatCard title={"Minimum Temperature"}
+                                  metric={`${results.current_weather.temperature?.toFixed(1)} °C`}
+                                  color={"blue"}
+                        />
+
+
+                        <div>
+                            <StatCard title={"UV Index"}
+                                      metric={results.current_weather.temperature?.toFixed(1)}
+                                      color={"red"}
+                            />
+                            {
+                                Number(4) > 2 && <ColloutCard message={"UV Index is too high wear a SPF"} warning/>
+                            }
+                        </div>
+                        <div className={"flex gap-4"}>
+                            <StatCard title={"Wind Direction"}
+                                      metric={results.current_weather.winddirection?.toFixed(1)}
+                                      color={"cyan"}
+                            />
+                            <StatCard title={"Wind Speed"}
+                                      metric={`${results.current_weather.windspeed?.toFixed(1)} m/s`}
+                                      color={"violet"}
+                            />
+
+                        </div>
+                    </div>
+
+                    <TempChart className={" p-4 lg:px-10"} results={results}/>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default weather;
